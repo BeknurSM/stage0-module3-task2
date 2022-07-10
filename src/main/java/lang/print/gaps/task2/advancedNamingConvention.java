07c1c37f691158c79e0b794c39b362b29b9a8da5
BekoSm <b.smagulov.1999@gmail.com>
2022-07-10 19:37:15 +0600

    Pushed

--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -1,16 +1,16 @@
 package lang.print.gaps.task2;
 
 public class advancedNamingConvention {
-    private static final int adult_age = 18;
+     private static final int adult_age = 18;
+     int age;
+     int phoneNumber;
 
-    private int AGE;
-    private int phoneNumber;
-
-    void CAllToFriend() {
+    public static void callToFriend() {
+        System.out.println(adult_age);
         //some code
-    }
 
-    void callByNumber(int Number) {
+    }
+    void callByNumber(int Number){
         //some code
     }
 }
